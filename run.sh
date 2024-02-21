9a7048bd0cd6968cb0160ea09d6259d8d94e2762
school497 <84127815+school497@users.noreply.github.com>
2024-02-21 10:50:42 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,4 +8,6 @@ curl -O https://raw.githubusercontent.com/school497/facepos/main/main.py
 curl -O https://raw.githubusercontent.com/school497/facepos/main/civilian.py
 curl -O https://raw.githubusercontent.com/school497/facepos/main/business.py
 curl -O https://raw.githubusercontent.com/school497/facepos/main/bank.py 
+chmod 777 ~/Library/Application\ Support/FacePOS
+chmod 777 *
 python3 ~/Library/Application\ Support/FacePOS/main.py
