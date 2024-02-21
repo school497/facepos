9a61e414f611d9a1f0ff401145ffa390dc71adea
school497 <84127815+school497@users.noreply.github.com>
2024-02-21 09:37:16 +0900

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,11 @@
+cd ~/Library/Application\ Support
+mkdir FacePOS
+cd ~/Library/Application\ Support/FacePOS
+mkdir faces
+mkdir balances
+mkdir business
+curl -O https://raw.githubusercontent.com/school497/facepos/main/main.py
+curl -O https://raw.githubusercontent.com/school497/facepos/main/civilian.py
+curl -O https://raw.githubusercontent.com/school497/facepos/main/business.py
+curl -O https://raw.githubusercontent.com/school497/facepos/main/bank.py 
+python3 ~/Library/Application\ Support/FacePOS/main.py
